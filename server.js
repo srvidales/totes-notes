d331b60979ca06ed92894a749c430f1c1572af89
Sergio Vidales Perez <srvidales@gmail.com>
2023-10-02 11:02:04 -0700

    Add basic express implementation and uuidv4

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const uuidv4 = require('uuidv4');
+const app = express();
+const PORT = 3001;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.send('GET /api/notes response.')
+});
+
+app.post('/api/notes', (req, res) => {
+    const uuid = uuidv4.uuid();
+    res.send(`POST /api/notes ${uuid} response`)
+});
+
+app.listen(PORT, () =>
+    console.log(`Example app listening at http://localhost:${PORT}`)
+);

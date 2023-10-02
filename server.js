066b1dba0042409f5b18202a2e48615d658fbe4a
Sergio Vidales Perez <srvidales@gmail.com>
2023-10-02 12:18:23 -0700

    Implement /api/notes GET, POST, DELETE

--- server.js
+++ server.js
@@ -1,26 +1,52 @@
 const express = require('express');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+const noteData = require('./db/db.json');
 const app = express();
 const PORT = 3001;
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
-  });
+});
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
-    res.send('GET /api/notes response.')
+    res.json(noteData);
 });
 
 app.post('/api/notes', (req, res) => {
-    const uuid = uuidv4.uuid();
-    res.send(`POST /api/notes ${uuid} response`)
+    const { title, text } = req.body;
+    const tmpNote = { id: uuidv4(), title, text }
+    tmpNote.id = uuidv4();
+    noteData.push(tmpNote);
+    fs.writeFile(path.join(__dirname, 'db/db.json'), JSON.stringify(noteData, null, 2), (err) => {
+        if (err) {
+            res.status(500).json({ error: 'Error saving JSON data to file.' });
+        } else {
+            res.json(tmpNote);
+        }
+    });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    const noteIndex = noteData.findIndex((note) => note.id === req.params.id);
+    const tmpNote = noteData[noteIndex];
+    noteData.splice(noteIndex, 1);
+    fs.writeFile(path.join(__dirname, 'db/db.json'), JSON.stringify(noteData, null, 2), (err) => {
+        if (err) {
+            res.status(500).json({ error: 'Error saving JSON data to file.' });
+        } else {
+            res.json(tmpNote);
+        }
+    });
 });
 
 app.listen(PORT, () =>
